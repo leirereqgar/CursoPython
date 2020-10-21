3ff4222886c3c7628386a9a2de69b624852ff53c
leirereqgar <leirereqgar@gmail.com>
2020-10-21 14:41:38 +0200

    Inicio del curso

---
+++ edad.py
@@ -0,0 +1,3 @@
+edad = int(input("¿Cual es tu edad? "))
+edad -= 10
+print("Vaya parece que tienes " + str(edad))
---
+++ holamundo.py
@@ -0,0 +1,5 @@
+print("¿Como te llamas?")
+
+nombre = input()
+
+print("Hola " + nombre)
